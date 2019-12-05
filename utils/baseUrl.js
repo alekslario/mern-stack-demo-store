b5c197b2422b52ca1a741cebb705fa1149ac6684
unknown <aleksandlario@gmail.com>
2019-12-05 04:10:18 +0000

    Fixed url error

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "react-next-demo.now.sh"
+    ? "https://react-next-demo.now.sh"
     : "http://localhost:3000";
 
 export default baseUrl;
