7097b442039f00c1f6329faf81e474330cbe2804
unknown <aleksandlario@gmail.com>
2019-12-06 01:41:37 +0000

    New url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://next-react.now.sh"
+    ? "https://mern-stack-demo-store.now.sh"
     : "http://localhost:3000";
 
 export default baseUrl;
